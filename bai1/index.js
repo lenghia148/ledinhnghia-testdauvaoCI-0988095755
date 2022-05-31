2515df80676ff4dafbef265689b461f3be681cd1
lenghia148 <ledinhnghia.qb@gmail.com>
2022-05-31 22:23:31 +0700

    edit

--- bai1/index.js
+++ bai1/index.js
@@ -1,35 +1,37 @@
-let numberA = document.querySelector(".number-a")
-let numberB = document.querySelector(".number-b")
-let button = document.querySelector(".button")
-let primeNumber = document.querySelector(".prime-number")
-let prime = ""
+let numberA = document.querySelector(".number-a");
+let numberB = document.querySelector(".number-b");
+let button = document.querySelector(".button");
+let primeNumber = document.querySelector(".prime-number");
+let prime = "";
 
-button.addEventListener("click",()=>
-{
-for (let i = numberA.value; i<=numberB.value; i++)
-        {
-             if (primes(i))
-             {
-                prime+= `${i}, `
-             }
-        }
-
-        primeNumber.innerText = prime;
-})
-
-function primes(number)
-{
-    if(number<2)
-    {
-        return false;
+button.addEventListener("click", () => {
+    prime = "";
+  let a = Number(numberA.value);
+  let b = Number(numberB.value);
+if (a>0&&b>0&&a<b)
+ { for (let i = a; i <= b; i++) {
+    if (primes(i)) {
+      prime += `${i}, `;
     }
-    
-    for (let i = 2 ; i <= Math.sqrt(number);i++)
-    {
+  }
 
-        if (number % i ===0)
-        {
-            return false
-        }
-    }return true;
+  primeNumber.innerText = prime;
+}
+else
+{
+    primeNumber.innerText ="vui lòng nhập lại"
+}
+});
+
+function primes(number) {
+  if (number < 2) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
--- bai2/index.js
+++ bai2/index.js
@@ -12,5 +12,5 @@ function numberOneTriangle(number)
 	
 
 
-numberOneTriangle(4)
+numberOneTriangle(7)
  
