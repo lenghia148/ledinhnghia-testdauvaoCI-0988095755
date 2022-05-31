d7391fd2f4749abe2bdd7017929e1ad6f05b3463
lenghia148 <ledinhnghia.qb@gmail.com>
2022-05-31 20:15:08 +0700

    edit

---
+++ bai1/index.js
@@ -0,0 +1,18 @@
+let numberA = document.querySelector(".number-a")
+let numberB = document.querySelector(".number-b")
+let button = document.querySelector(".button")
+let primeNumber = document.querySelector(("prime-number"))
+let flag = 0;
+let prime = []
+for (j = 2 ; i<numberB.value; i++)
+        {
+               for (let j = 2 ; j<numberB.value ;j++)
+                {
+                    if ( i % j ==0 )
+                       { 
+                           continue;
+                       }
+                       
+                }
+
+        }
---
+++ bai2/index.js
@@ -0,0 +1,16 @@
+let star = "";
+function numberOneTriangle(number)
+{
+    
+
+    for(let i=1;i<=number;i++)
+    {
+	    star = star + '*'
+        console.log(star)
+	}
+}
+	
+
+
+numberOneTriangle(2)
+ 
